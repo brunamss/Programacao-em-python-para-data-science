f162ce02d7dbb6f8c36f67f85ad4e831d3f83fe4
MARI <brunamss54@gmail.com>
2026-08-19 17:54:09 -0300

    Add files via upload

--- atividade.py
+++ atividade.py
@@ -1,46 +1,75 @@
-VENDAS = {
-'SETOR1':{
-'MÊS 1':[150,200,300]},
-'SETOR2':{
-'MÊS 2':[300,250,300]},
-'SETOR3':{
-'MÊS 3':[15,20,300]},
-'SETOR4':{
-'MÊS 4':[150,2000,300000],
-}
-}
-
-
-
-# Você foi contratado para verificar qual setor vendeu mais
-
-
-
-setor_1 = sum( VENDAS['SETOR1']['MÊS 1'])
-setor_2 =  sum(VENDAS['SETOR2']['MÊS 2'])
-setor_3 =  sum(VENDAS['SETOR3']['MÊS 3'])
-setor_4 =  sum(VENDAS['SETOR4']['MÊS 4'])
-
-
-setores =  ['setor 1', 'setor 2', 'setor 3', 'setor 4']
-
-
-todas_vendas = []
-todas_vendas += (setor_1, setor_2,setor_3, setor_4)
-maior_venda =  max(todas_vendas)
-setor_q_mais_vendeu = todas_vendas.index(maior_venda)
-print('O SETOR QUE MAIS VENDEU - ', setores[setor_q_mais_vendeu])
 
 
 
 
-# qual a media total de vendas?
-soma  =  sum(todas_vendas)/len(todas_vendas)
-print('R$ total ', soma)
 
 
-# Qual a maior venda?
-print(maior_venda)
+
+# MINHA PARTE
 
 
-# Utilize as estruturas que você já conhece.
+
+pe =  input('Deseja acessar o banco? ')
+
+
+while pe  == 'sim':
+    senha  =  input('Senha: ')
+    if senha  == '123':
+        print('banco X')
+        print('''Menu:
+            
+            1 - saque
+            2 - deposito
+            3 - extrato
+            4 -  sair 
+            
+            
+            
+            ''')
+        saldo = 1000
+        saque = 0  
+        deposito = 0
+        op =  input('escolha a operação: ')
+        if  op == '1':
+            
+            saque = float (input ('saque: '))
+            saldo =saldo - saque
+            x = saldo
+            print (saldo)
+
+        if op == '2':
+            
+            deposito = float (input('deposito: '))
+            saldo =x + deposito
+            y = saldo
+            print (saldo)
+
+        if op == '3':
+           
+            extrato = [saldo, x, y]
+
+            # extrato.append(saque)
+            # extrato.append(deposito)
+            print(extrato)
+
+
+
+            pe =  input('Deseja acessar o banco? ')
+
+        if op  == '4':
+            print ('Obrigado pelo seu acesso')
+            exit()
+
+
+            
+            pass
+    else:
+        print('não te encontramos no nosso banco')        
+            
+
+
+
+
+# PARTE DA PROFESSORA
+
+
---
+++ aula4.py
@@ -0,0 +1,39 @@
+
+
+
+
+# finito = 
+for x in range(3):
+    login  = input('login: ')
+    senha =  input('senha: ')
+    if login == 'b' and senha =='1':
+        print('Produtos>')
+        produ = ['uva', 'pera', 'manga ']
+        print(produ)
+        break
+else:
+    print('bloquedo ...')    
+
+
+
+
+
+
+
+
+
+
+
+lista  =  [1,2,3]
+tupla  =  (4,5,6)
+d = {
+    'a':200,
+    'b':500,
+    'c': 1000
+    }
+for n in d.keys():
+    print(n)    
+
+
+
+    
